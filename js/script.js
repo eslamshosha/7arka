10bb3cee59c5713d179995a2300a5207a3e77c7e
eslam shosha <you@example.comeslamshosha22.es@gmail.com>
2021-08-24 04:22:15 +0200

    add internal pages

--- js/script.js
+++ js/script.js
@@ -131,6 +131,22 @@ $(document).ready(function() {
     } else {
         example.setAttribute("src", "images/logo.png");
     }
+
+    //search filter
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+        } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+        } 
+    });
+    }
 });
 
 
